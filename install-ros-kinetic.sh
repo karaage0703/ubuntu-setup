d25bb8fdfc93d76fa5cda2b5b1099ad6a3fe5b5e
karaage0703 <coldchicken@gmail.com>
2017-09-18 22:10:54 +0900

    bug fix and change permission

--- install-ros-kinetic.sh
+++ install-ros-kinetic.sh
@@ -1,16 +1,19 @@
-$ sudo sh -c 'echo "deb http://packages.ros.org/ros/ubuntu $(lsb_release -sc) main" > /etc/apt/sources.list.d/ros-latest.list'
-$ sudo apt-key adv —keyserver hkp://ha.pool.sks-keyservers.net:80 —recv-key 421C365BD9FF1F717815A3895523BAEEB01FA116
-$ sudo apt-get update
-$ sudo apt-get install -y ros-kinetic-desktop-full
-$ sudo rosdep init
-$ rosdep update
-$ sudo apt-get install -y python-wstool python-catkin-tools
-$ echo "source /opt/ros/kinetic/setup.bash" >> ~/.bashrc
-$ source ~/.bashrc
-$ sudo apt-get install -y python-rosinstall
-$ mkdir -p ~/catkin_ws/src
-$ cd ~/catkin_ws
-$ catkin init
-$ catkin build
-$ sh -c "echo \"source ~/catkin_ws/devel/setup.bash\" >> ~/.bashrc"
-$ source ~/.bashrc
+#!/bin/bash
+# modified by karaage0703
+
+sudo sh -c 'echo "deb http://packages.ros.org/ros/ubuntu $(lsb_release -sc) main" > /etc/apt/sources.list.d/ros-latest.list'
+sudo apt-key adv —keyserver hkp://ha.pool.sks-keyservers.net:80 —recv-key 421C365BD9FF1F717815A3895523BAEEB01FA116
+sudo apt-get update
+sudo apt-get install -y ros-kinetic-desktop-full
+sudo rosdep init
+rosdep update
+sudo apt-get install -y python-wstool python-catkin-tools
+echo "source /opt/ros/kinetic/setup.bash" >> ~/.bashrc
+source ~/.bashrc
+sudo apt-get install -y python-rosinstall
+mkdir -p ~/catkin_ws/src
+cd ~/catkin_ws
+catkin init
+catkin build
+sh -c "echo \"source ~/catkin_ws/devel/setup.bash\" >> ~/.bashrc"
+source ~/.bashrc
