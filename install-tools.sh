e8e14e02d40137dc0b81856b8aa26cd5c80058c2
karaage <coldchicken@gmail.com>
2020-11-04 22:24:16 +0900

    Change apt-get to apt

--- install-tools.sh
+++ install-tools.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
-sudo apt-get update
+sudo apt update
 
 # software development tool
-sudo apt-get -y install ssh
-sudo apt-get -y install chrony						# synchronized clock
-sudo apt-get -y install byobu							# virtual terminal
-sudo apt-get -y install vim
-sudo apt-get -y install vim-gtk
-sudo apt-get -y install kazam							# video capture
-sudo apt-get -y install rlwrap						# completion
-sudo apt-get -y install exuberant-ctags		# ctag
-sudo apt-get -y install chromium-browser	# google chrome
+sudo apt -y install ssh
+sudo apt -y install chrony  			# synchronized clock
+sudo apt -y install byobu				# virtual terminal
+sudo apt -y install vim                 # Editor
+sudo apt -y install vim-gtk             # Editor
+sudo apt -y install kazam				# video capture
+# sudo apt -y install rlwrap				# completion
+# sudo apt -y install exuberant-ctags		# ctag
+sudo apt -y install chromium-browser	# google chrome
+sudo apt -y install arandr	            # multi display
