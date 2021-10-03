b8b95cdb526407d171e0194acb6bd78702484243
karaage0703 <coldchicken@gmail.com>
2021-10-03 09:55:01 +0900

    Add setup script

---
+++ install-pyenv.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+# Reference sites
+
+git clone https://github.com/pyenv/pyenv.git ~/.pyenv
+echo 'export PYENV_ROOT="$HOME/.pyenv"' >> ~/.bashrc
+echo 'export PATH="$PYENV_ROOT/bin:$PATH"' >> ~/.bashrc
+echo 'eval "$(pyenv init -)"' >> ~/.bashrc
+source ~/.bashrc
+git clone https://github.com/pyenv/pyenv-virtualenv.git $(pyenv root)/plugins/pyenv-virtualenv
+echo 'eval "$(pyenv virtualenv-init -)"' >> ~/.bashrc
+echo export PYENV_VIRTUALENV_DISABLE_PROMPT=1 >> ~/.bashrc
+source ~/.bashrc
+
