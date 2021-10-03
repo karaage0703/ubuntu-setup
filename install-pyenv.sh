91ab1b72c692c9174a266d558dd0af7ac50b20dd
karaage <coldchicken@gmail.com>
2021-10-03 11:36:02 +0900

    Update install-pyenv.sh

--- install-pyenv.sh
+++ install-pyenv.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # Reference sites
-https://github.com/pyenv/pyenv/wiki#suggested-build-environment
+# https://github.com/pyenv/pyenv/wiki#suggested-build-environment
 
 # Install tools for building python
 sudo apt update; sudo apt install -y make build-essential libssl-dev zlib1g-dev libbz2-dev libreadline-dev libsqlite3-dev wget curl llvm libncursesw5-dev xz-utils tk-dev libxml2-dev libxmlsec1-dev libffi-dev liblzma-dev
