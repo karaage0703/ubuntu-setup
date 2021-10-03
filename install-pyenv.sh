330026b3d915598290788258b7da0422529a1e6d
karaage0703 <coldchicken@gmail.com>
2021-10-03 10:55:19 +0900

    Update script for building python

--- install-pyenv.sh
+++ install-pyenv.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 # Reference sites
+https://github.com/pyenv/pyenv/wiki#suggested-build-environment
+
+# Install tools for building python
+sudo apt update; sudo apt install -y make build-essential libssl-dev zlib1g-dev libbz2-dev libreadline-dev libsqlite3-dev wget curl llvm libncursesw5-dev xz-utils tk-dev libxml2-dev libxmlsec1-dev libffi-dev liblzma-dev
 
 git clone https://github.com/pyenv/pyenv.git ~/.pyenv
 echo 'export PYENV_ROOT="$HOME/.pyenv"' >> ~/.bashrc
