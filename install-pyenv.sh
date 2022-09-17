03a60b0d7a28b7c1e7c962161117b87d7d37f37e
karaage0703 <coldchicken@gmail.com>
2022-09-17 10:36:48 +0900

    Update for setup

--- install-pyenv.sh
+++ install-pyenv.sh
@@ -11,7 +11,7 @@ echo 'export PATH="$PYENV_ROOT/bin:$PATH"' >> ~/.bashrc
 echo 'eval "$(pyenv init --path)"' >> ~/.bashrc
 echo 'eval "$(pyenv init -)"' >> ~/.bashrc
 source ~/.bashrc
-git clone https://github.com/pyenv/pyenv-virtualenv.git $(pyenv root)/plugins/pyenv-virtualenv
+git clone https://github.com/pyenv/pyenv-virtualenv.git ~/.pyenv/plugins/pyenv-virtualenv
 echo 'eval "$(pyenv virtualenv-init -)"' >> ~/.bashrc
 echo export PYENV_VIRTUALENV_DISABLE_PROMPT=1 >> ~/.bashrc
 source ~/.bashrc
