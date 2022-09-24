29029ef7f208a627dd418af991e14fbe9794ede5
karaage0703 <coldchicken@gmail.com>
2022-09-24 21:06:37 +0900

    Add docker compose

--- install-docker.sh
+++ install-docker.sh
@@ -10,5 +10,6 @@ sudo apt-key fingerprint 0EBFCD88
 sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
 sudo apt-get update
 sudo apt-get -y install docker-ce docker-ce-cli containerd.io
+sudo apt-get -y install docker-compose-plugin
 sudo gpasswd -a $USER docker
 # Log off and Log in
